7e93310aa8e81267d69da132c889424c9365fc24
DulatUtebolatov <61288775+DulatUtebolatov@users.noreply.github.com>
2024-01-12 08:08:48 +0600

    app.js

--- app.js
+++ app.js
@@ -409,7 +409,7 @@ dtn6.addEventListener("click", function(){
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData('Order :' + result.toString());
+	tg.sendData('' + result.toString() + '","menu":"coffe-latte');
 });
 
 
