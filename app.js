7b9483a3be84a7ebacaea0679e1e7698f5d7de31
DulatUtebolatov <61288775+DulatUtebolatov@users.noreply.github.com>
2024-01-14 07:41:27 +0600

    app.js

--- app.js
+++ app.js
@@ -409,11 +409,12 @@ dtn6.addEventListener("click", function(){
 });
 
 let finorder = "";
-Telegram.WebApp.addEventListener("mainButtonClicked", function(){
-//Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	Telegram.WebApp.showAlert("Salem" + Telegram.WebApp.WebAppUser.username.toString());
-	window.location.href = 'BlankAdress.html';
-	tg.sendData("[" + result.toString() + "," + val1.toString() + "," + val2.toString() + "," + val3.toString() + "," + val4.toString() + "," + val5.toString() + "," + val6.toString() + "]");
+//Telegram.WebApp.addEventListener("mainButtonClicked", function(){
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	val1 = val1+1;
+	//Telegram.WebApp.showAlert("Salem" + Telegram.WebApp.WebAppUser.username.toString());
+	//window.location.href = 'BlankAdress.html';
+	//tg.sendData("[" + result.toString() + "," + val1.toString() + "," + val2.toString() + "," + val3.toString() + "," + val4.toString() + "," + val5.toString() + "," + val6.toString() + "]");
 
 });
 
