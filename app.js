3f481d9dd7504c0e89c06b7ac60002e0f8c81673
DulatUtebolatov <61288775+DulatUtebolatov@users.noreply.github.com>
2024-01-14 12:05:58 +0600

    app.js

--- app.js
+++ app.js
@@ -422,7 +422,8 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 
 let timer = setInterval(function() {
   if(bet==1) {
-	  window.location.href = 'BlankAdress.html';
+	  document.getElementById('page01').style.display='none'; 
+	  document.getElementById('page02').style.display='block';
 	  tg.MainButton.hide();
   }
 	else {
