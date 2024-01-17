f98a30d42828273a6446154a4e5e9b445a24e140
DulatUtebolatov <61288775+DulatUtebolatov@users.noreply.github.com>
2024-01-17 08:40:36 +0600

    app.js

--- app.js
+++ app.js
@@ -497,8 +497,8 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	if (bet == 1){
 	orderprice.innerHTML = "Ваш заказ на сумму: " + result.toString() + "тг";
 	pageone.style.display = "none";
-	pagetwo.style.display = "block";
-	pagetree.style.display = "none";
+	pagetwo.style.display = "none";
+	pagetree.style.display = "block";
 	tg.MainButton.setText("Подтвердить и оплатить");
 	bet = 2;
 	}
